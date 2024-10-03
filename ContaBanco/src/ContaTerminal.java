d5a4f9e306dbb8a5b10c5bb068e14f7e2cd9b8ef
Bruno Gomes Laurentino <bruno.laurentino.adm@gmail.com>
2024-10-03 09:42:32 -0300

    Concluindo o desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Informe o número da conta: ");
+        int conta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Informe o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        double saldo = 237.48;
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua Agência é: Agência: " + agencia + " Conta: " + conta + " e seu saldo é Saldo: " + saldo );
+
     }
 }
